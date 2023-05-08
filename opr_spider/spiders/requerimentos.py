40102861a989feb29cc3dc2fb88fc5dd8f3f3422
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2023-05-08 14:39:59 -0300

    opr_spider/spiders/requerimentos.py: add urls to make the requests

--- opr_spider/spiders/requerimentos.py
+++ opr_spider/spiders/requerimentos.py
@@ -6,15 +6,13 @@ class RequerimentosSpider(scrapy.Spider):
     name = "requerimentos"
 
     def start_requests(self):
-        urls = [
-            "https://quotes.toscrape.com/page/1/",
-            "https://quotes.toscrape.com/page/2/",
-        ]
+        urls = [f"https://cmbelem.pb.gov.br/public/portal/requerimentos/geral?page={i}" for i in range(1, 23)]
+        
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = f"quotes-{page}.html"
+        page = response.url.split("=")[-1]
+        filename = f"requerimentos-{page}.html"
         Path(filename).write_bytes(response.body)
         self.log(f"Saved file {filename}")
