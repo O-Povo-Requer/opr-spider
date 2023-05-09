d826fb9076cab24363400015dbfe16a4dfbb0d2f
Carlos Ribeiro <carlos.ribeiro@ccc.ufcg.edu.br>
2023-05-09 09:51:05 -0300

    scrape requerimentos titles

---
+++ opr_spider/spiders/informacoes.py
@@ -0,0 +1,21 @@
+import scrapy
+import csv
+
+class InformacoesSpider(scrapy.Spider):
+    name = "informacoes"
+
+    def start_requests(self):
+        with open("requerimentos.csv", newline="") as csvfile:
+            reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+            next(reader, None)  # skip the headers
+            for row in reader:
+                yield scrapy.Request(url=row[0], callback=self.parse)
+
+    def parse(self, response):
+        yield {
+            'titulo': self.get_titulo(response)
+        }
+
+    def get_titulo(self, response):
+        tag = response.xpath("//title/text()")
+        return tag.get().split(" | ")[0]
